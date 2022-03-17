e0217d75a71f2c6e400a8854875911812a46d09b
wellers <paul_welbourne@yahoo.co.uk>
2022-03-17 23:39:50 +0000

    Extract consumer into separate container.

---
+++ node-consumer/lib/server.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const express = require('express');
+const amqplib = require('amqplib');
+
+const PORT = 4000;
+
+const amqp_url = process.env.CLOUDAMQP_URL || 'amqp://192.168.50.101:5672';
+
+const queue = 'test_queue';
+
+async function boot() {	
+	const connection = await amqplib.connect(amqp_url, "heartbeat=60");
+	const channel = await connection.createChannel();
+	
+	await channel.assertQueue(queue, { durable: true });		
+	
+	await channel.consume(queue, async (message) => {
+		if (message !== null) {
+			console.log(`Message received: ${message.content.toString()}`);
+			channel.ack(message);				
+		}
+	});
+	
+	const app = express();
+
+	app.get('/status', (_, res) => {
+		return res.json({
+			start: Date.now()
+		});
+	});
+	
+	app.listen(PORT, async () => console.log(`Node server ready on ${PORT}`));
+}
+boot();
---
+++ node-producer/lib/server.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const express = require('express');
+const amqplib = require('amqplib');
+
+const PORT = 80;
+
+const app = express();
+
+const amqp_url = process.env.CLOUDAMQP_URL || 'amqp://192.168.50.101:5672';
+
+const queue = 'test_queue';
+const exchange = 'test_exchange';	
+const route = 'test_route';
+
+app.get('/produce', async ({ query }, res) => {	
+	if (!query.message) {
+		res.json({ success: false, message: 'message is required.' });
+		return;
+	}
+
+	const { message } = query;
+	
+	const connection = await amqplib.connect(amqp_url, "heartbeat=60");
+
+	const channel = await connection.createChannel();		
+	await channel.assertExchange(exchange, 'direct', { durable: true });
+	await channel.assertQueue(queue, { durable: true });
+	await channel.bindQueue(queue, exchange, route);
+
+	channel.publish(exchange, route, Buffer.from(message));
+
+	setTimeout(() => {
+		channel.close();
+		connection.close();
+	}, 500);
+
+	res.json({ success: true, message: 'Message queued.' });	
+});
+
+app.get('/status', (_, res) => {
+	return res.json({
+		start: Date.now()
+	});
+});
+
+app.listen(PORT, async () => console.log(`Node server ready on ${PORT}`));
