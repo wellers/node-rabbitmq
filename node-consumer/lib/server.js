1abe2d89e651a056abe29c3f4fdcf0442135a14a
wellers <paul_welbourne@yahoo.co.uk>
2022-04-07 14:07:57 +0100

    Add docker-compose.yaml.

--- node-consumer/lib/server.js
+++ node-consumer/lib/server.js
@@ -11,16 +11,18 @@ const amqp_url = process.env.CLOUDAMQP_URL || 'amqp://192.168.50.101:5672';
 const queue = 'test_queue';
 
 async function boot() {	
-	const app = express();
+	const app = express();	
 
 	app.get('/status', (_, res) => res.json({ start: Date.now() }));
 	
 	app.listen(PORT, async () => console.log(`Node server ready on ${PORT}`));
 
-	const connection = await amqplib.connect(amqp_url, "heartbeat=60");
+	console.log('Waiting for messages...');
+
+	const connection = await amqplib.connect(amqp_url, "heartbeat=60");	
 
 	const channel = await connection.createChannel();	
-	await channel.assertQueue(queue, { durable: true });	
+	await channel.assertQueue(queue, { durable: true });
 	await channel.consume(queue, async (message) => {
 		if (message !== null) {
 			console.log(`Message received: ${message.content.toString()}`);
